671fe151517409b992475b5ba4698ac0fb4e2e19
Gitanjali Mete <metegita8@gmail.com>
2025-05-10 10:58:38 +0530

    update

--- src/main.jsx
+++ src/main.jsx
@@ -20,7 +20,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
-import store from './Store.js'
+import store from './store.js'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* Wrap Provider inside StrictMode */}
